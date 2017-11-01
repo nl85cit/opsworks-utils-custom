366260161f5dff19a9422d70b087e58480c24aef
nl85cit <ned.lacelle@cornell.edu>
2017-11-01 14:39:04 -0400

    packages multi OS update

--- opsworks-utils-cookbook/recipes/packages.rb
+++ opsworks-utils-cookbook/recipes/packages.rb
@@ -1,1 +1,16 @@
-yum_package 'nano'
+package_install_array =  ['nano', 'emacs']
+
+package_install_array.each do |this_package|
+  case node[:platform]
+  when "amazon"
+    rpm_package "#{this_package}"
+  when "redhat"
+    rpm_package "#{this_package}"
+  when "ubuntu"
+    dpkg_package "#{this_package}"
+  when "suse"
+    zypper_package "#{this_package}"
+  else
+    rpm_package "#{this_package}"
+ end
+end
